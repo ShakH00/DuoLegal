6e482f10c67668118c6f56b4e30730c0dff5e148
Shak H <shakil.hussain2022@gmail.com>
2024-11-09 16:50:47 -0700

    updates to ui

--- static/script.js
+++ static/script.js
@@ -1,5 +1,6 @@
-// Scroll bar top
+// Scroll progress bar (top)
 window.onscroll = function () {
+    buttonFunction();
     myFunction();
 };
 
@@ -9,3 +10,27 @@ function myFunction() {
     var scrolled = (winScroll / height) * 100;
     document.getElementById("myBar").style.width = scrolled + "%";
 }
+
+// Back to top button
+var btn = document.getElementById("button");
+
+function buttonFunction() {
+    var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+    var scrollHeight = document.documentElement.scrollHeight;
+    var clientHeight = document.documentElement.clientHeight;
+
+    if (scrollTop > 100 && scrollHeight - clientHeight - scrollTop > 50) {
+        btn.classList.add('show');
+    } else {
+        btn.classList.remove('show');
+    }
+}
+
+btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: 0
+    }, 10);
+});
+
+window.addEventListener('scroll', buttonFunction);
