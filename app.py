575b1399f6149c1655739f33ddce64cb2428679a
Abdulrahman Negmeldin <abdulrahman.m.negmeldin@gmail.com>
2024-11-09 16:49:58 -0700

    fixed login redirect

--- app.py
+++ app.py
@@ -6,41 +6,43 @@ app.secret_key = 'boi!#@$f23%^$^5u98pb7v9bu(*&*($^)(989540svirfuyvityr'
 #temporary boolean
 logged = False
 #home directory stuff
+
 @app.route('/')
 def home():
-    if logged == True:
+    if 'email' in session:
+        print("in session")
         return render_template('home.html', username=session['email'])
     else:
         return render_template('index.html')
 
-@app.route('/login.html', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    global logged
     if request.method == 'POST':
-        username = request.form['email']
-        pwd = request.form['password']
-        logged = True
-        return redirect(url_for('home'))
-    else:
-        return render_template('login.html', error='Invalid email or password!')
+        username = request.form.get('email')
+        pwd = request.form.get('password')
 
-    #add code to get from mongo db
-        #if user and pwd = user[1]:
-            #session['email'] = user[0]
-            #return redirect(url_for('home'))
-        #else
-            #return render_template('login.html', error='Invalid email or password!')
+        # Simulated login check (replace with actual database verification)
+        if username and pwd:  # Simulate successful login
+            session['email'] = username
+            return redirect(url_for('home'))
+        else:
+            return render_template('login.html', error='Invalid email or password!')
     return render_template('login.html')
-@app.route('/register.html', methods=['GET', 'POST'])
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    global logged
     if request.method == 'POST':
-        username = request.form['email']
-        pwd = request.form['password']
-        logged = True
-        #code to add to mongo db
-        return(redirect(url_for('login')))
+        username = request.form.get('email')
+        pwd = request.form.get('password')
+        # Simulate user registration
+        session['email'] = username  # Log the user in after registration
+        return redirect(url_for('login'))
     return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('email', None)
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
